edcfce4cb2c8f0351fe5740bfe2492427a667c30
Abouerp <1057240821@qq.com>
2020-07-18 10:43:25 +0800

    deploy

---
+++ deploy/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+git fetch && \
+git reset --hard origin/master
+
+chmod +x ./gradlew && \
+./gradlew jibDockerBuild
+docker-compose up -d
+docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
+
+echo "deploy success"
