714e02f3f6c9e1871848350406bb0965bba39805
Abouerp <1057240821@qq.com>
2020-07-20 09:44:57 +0800

    polish

--- api/src/main/java/zsc/edu/abouerp/api/ApiApplication.java
+++ api/src/main/java/zsc/edu/abouerp/api/ApiApplication.java
@@ -2,10 +2,12 @@ package zsc.edu.abouerp.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "zsc.edu.abouerp")
+@EntityScan(basePackages ={ "zsc.edu.abouerp.entity"})
 public class ApiApplication {
 
     public static void main(String[] args) {
