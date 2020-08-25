7db2f4dfc0c4f42beb8ff3cb65b0f85ad829dc2c
Abouerp <1057240821@qq.com>
2020-08-25 11:11:29 +0800

    add Resign of FindAll

--- api/src/main/java/zsc/edu/abouerp/api/init.java
+++ api/src/main/java/zsc/edu/abouerp/api/init.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import zsc.edu.abouerp.entity.domain.Administrator;
 import zsc.edu.abouerp.entity.domain.Authority;
+import zsc.edu.abouerp.entity.domain.PersonnelStatus;
 import zsc.edu.abouerp.entity.domain.Role;
 import zsc.edu.abouerp.service.repository.AdministratorRepository;
 import zsc.edu.abouerp.service.repository.RoleRepository;
@@ -41,6 +42,7 @@ public class init implements CommandLineRunner {
                     .setAccountNonExpired(true)
                     .setAccountNonLocked(true)
                     .setCredentialsNonExpired(true)
+                    .setStatus(PersonnelStatus.IN_OFFICE)
                     .setEnabled(true)
                     .setRoles(Stream.of(role).collect(Collectors.toSet()));
             administratorRepository.save(administrator);
